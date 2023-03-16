b95c73c98c6332fe3b21e769f50e29ee8d53ef82
rakhasf <rakhasaptafabian@gmail.com>
2023-03-16 14:34:11 +0700

    git commit

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,14 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Comment;
 
 class CommentController extends Controller
 {
     public function store(Request $request)
     {
         $request -> validate([
-            'post_id' => 'required}exist:posts,id',
-            'comment_content' => 'required',
+            'post_id' => 'required|exists:posts,id',
+            'comments_content' => 'required',
         ]);
+
+        $request['user_id'] = auth()->user()->id;
+
+        $comment = Comment::create($request->all());
+
+        return response()->json($comment);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
